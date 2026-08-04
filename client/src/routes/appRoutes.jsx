23e45b93a81a80f32a80844ed4cfff1dc0be635b
HadiaShahid0 <Hadia.Shahid@arhamsoft.com>
2026-08-04 18:11:29 +0500

    Create login page

--- client/src/routes/appRoutes.jsx
+++ client/src/routes/appRoutes.jsx
@@ -3,15 +3,21 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../features/auth/pages/Login";
 import Register from "../features/auth/pages/Register";
 
+import ChatPage from "../features/chat/pages/chatPage";
 const AppRoutes = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<Login />} />
+    return (
 
-      <Route path="/login" element={<Login />} />
+<Routes>
 
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <Route path="/" element={<Login />} />
+
+    <Route path="/login" element={<Login />} />
+
+    <Route path="/register" element={<Register />} />
+
+    <Route path="/chat" element={<ChatPage />} />
+
+</Routes>
   );
 };
 
